9882da89a0716eba808bd00f905f7b44b9991fea
Aleksandr <dragin.alex@gmail.com>
2024-09-24 22:53:18 +0200

    step 4

--- src/main/java/org/adrahin/contactsbook/controllers/DemoController.java
+++ src/main/java/org/adrahin/contactsbook/controllers/DemoController.java
@@ -1,10 +1,9 @@
 package org.adrahin.contactsbook.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/demo")
@@ -13,6 +12,18 @@ public class DemoController {
     @GetMapping("/{id}")
     public ResponseEntity<String> getItemById(@PathVariable("id") String value) {
         String response = String.format("Передано значение: %s", value);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PostMapping("/postItem")
+    public ResponseEntity<String> postItemWithValue(@RequestBody String data) {
+        String response = String.format("postItemWithValue получил данные: %s", data);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> postItem(@RequestBody String data) {
+        String response = String.format("postItem получил данные: %s", data);
         return ResponseEntity.ok(response);
     }
 }
